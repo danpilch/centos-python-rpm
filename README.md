73fbafc9705594084625498d497a8f6a75b58ec4
Dan Pilch <danielpilch@hotmail.co.uk>
2016-03-20 12:28:26 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # centos-python-rpm
-Latest Python for CentOS 7
+
+Latest Python RPM for CentOS 7 
+
+This script will build the latest v2 Python `v2.7.11`
+
+## Installation
+
+The simplest way to get up and running is to execute `./build.sh`. This will build an RPM called `python-latest-2.7.11-1.x86_64.rpm`. You can then install it via `yum` or `rpm`.
---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Build vars
+WORKING_DIR=/tmp/python_build
+PACKAGE_DIR=/tmp/python_package
+PYTHON_DIR="Python-2.7.11"
+PYTHON_TAR="https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz"
+BIN_DIR=`pwd`
+
+# Version info
+RPM_PACKAGE_NAME="python-latest"
+RPM_PACKAGE_VERSION="2.7.11"
+
+echo "Installing deps"
+sudo yum -y install zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel gem ruby-devel
+sudo gem install fpm
+
+echo "Creating build dirs"
+mkdir -p $WORKING_DIR
+mkdir -p $PACKAGE_DIR
+
+echo "Grabbing Python"
+curl $PYTHON_TAR | tar xvz -C $WORKING_DIR
+
+echo "Moving to Python working dir"
+cd $WORKING_DIR/$PYTHON_DIR
+
+echo "Configure Python source"
+./configure
+
+echo "Make Python source"
+make
+
+echo "Install Python binary to packaging dir"
+make install DESTDIR=$PACKAGE_DIR
+
+echo "Packaging Python as RPM via fpm"
+fpm -s dir -t rpm -n $RPM_PACKAGE_NAME -v $RPM_PACKAGE_VERSION -C $PACKAGE_DIR -p $BIN_DIR
